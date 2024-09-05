69f626d7d5faaa774bc5fd8845a1e33b35e466c4
Shakawath28 <shakawath132@gmail.com>
2024-09-06 01:02:08 +0600

    Uploaded first file

---
+++ Class1.js
@@ -0,0 +1,139 @@
+// 
+// var a=100
+// var name="Your score is"
+// var m=true
+// console.log(typeof(a))
+// console.log(typeof(name))
+// console.log(typeof(m))
+// 
+//  var a=toString(4)
+//  var b=toString(5)
+//  c=toString(true)
+//  console.log("The data type of variable a is : ",typeof(a))
+//  console.log("The data type of variable b is : ",typeof(b))
+//  console.log("The data type of variable c is : ",typeof(c))
+// 
+// 
+// var a=1000
+// var c=300
+// console.log(("The remainder is:") ,a%c)
+// 
+ var prompt = require('prompt-sync')();
+// var name=prompt("Enter your age=")
+// if (name==="Tamim") {
+//     console.log("Welcome!!")
+    
+// } else if (name==="shakawat") {
+//     console.log("Welcome!!")
+    
+// } else if(name==="hossain") {
+//     console.log("Welcome!!")
+    
+// } else{
+//     console.log("Sorry! We don’t find you in the system.")
+    
+// }
+// 
+// 
+// var year=Number(prompt("Enter a year="))
+// if (year%4==0 & year%100!=0) {
+//     console.log("Leap Year")
+    
+// } else if (year%400==0) {
+    
+//     console.log("Leap Year")
+// } else {
+//     console.log("This is not a Leap Year")
+    
+// }
+// var a=Number(prompt("Enter the price of first product:"))
+// var b=Number(prompt("Enter the price of 2nd product:"))
+// var c=Number(prompt("Enter the price of 3rd product:"))
+// T=(a+b+c)
+// if (T>50 & T<100) {
+//     console.log("Final Cost is:",D=(T-(T*0.1)))
+    
+// } 
+// else if (T>100) {
+//     console.log("Final Cost is:",D=(T-(T*0.2)))
+    
+// }
+// 
+// var a =50
+// var v=24
+// c=a%v
+// console.log("The remainder is",c)
+// var a= 20
+// var c=4
+// u=a%c
+// console.log("remainder is",u)
+// var name=prompt("Enter Your Name: ")
+// if (name=="Asif") {
+//     console.log("Welcome",name)
+ 
+// } 
+// else if(name=="Emon") 
+// {
+//     console.log("Welcome",name)
+    
+// }
+// else if(name=="Tamim")
+// {
+//     console.log("Welcome",name)
+// } else{
+//     console.log(name,"You are not in our System")
+// }
+
+// var year=Number(prompt("Enter a Year: "))
+// if (year%4==0 & year%100!=0) {
+//     console.log("Leap Year")
+    
+// }
+// else if(year%400==0)
+//     {
+//         console.log("Leap Year")
+//     }
+//     else{
+//         console.log("This is not a Leap Year")
+//     }
+
+// var a=Number(prompt("Enter the price of first product:"))
+// var b=Number(prompt("Enter the price of second product:"))
+// var c=Number(prompt("Enter the price of third product:"))
+// T=a+b+c
+// if(T>=50 & T<100)
+//     {
+//         console.log("Final Cost is",T-(T*0.1))
+//     }
+//     else if(T>100)
+//         {
+//             console.log("Final cost is",T-(T*0.2))
+//         }
+
+// var a=Number(prompt("x:"))
+// var b=Number(prompt("y:"))
+// var c=Number(prompt("z:"))
+// var d=Number(prompt("m:"))
+// var e=Number(prompt("n:"))
+// if (a>b && a>c && a>d && a>e) {
+//     console.log("a is the large number")
+    
+// } 
+// else if(b>a && b>c && b>d && b>e) 
+// {
+//     console.log("b is large number")
+
+// }
+// else if(c>a && c>b && c>d && c>e)
+// {
+//     console.log("c is the large number")
+// }
+// else if(d>a && d>b && d>c && d>e)
+// {
+//     console.log("d is the large number")
+// }
+
+//  else {
+//     console.log("e is the large number")
+// }
+
