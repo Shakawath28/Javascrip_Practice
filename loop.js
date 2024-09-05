2107b538543beeb5cd8a97776eaba0cddb76b4e1
Shakawath28 <shakawath132@gmail.com>
2024-09-06 01:05:57 +0600

    Uploaded fourth file

---
+++ loop.js
@@ -0,0 +1,70 @@
+// while(condition)
+//     {
+//         loop body
+//     }
+
+
+// var counter=0
+// while(counter<5)
+//     {
+//         console.log("Hello")
+//         counter++
+//     }
+
+// var counter=0
+// while(counter<5)
+//     {
+//         counter+=2 
+//         console.log(counter,"Hello world")
+        
+//     }
+    
+// var count=1
+// for(; count<=5; count++)
+//     {
+//         console.log("Hello",count)
+//     }
+
+// var sum=0
+// for(var i=1;i<=10;i++)
+// {
+    
+//     console.log(sum,"+",i,"=",sum+i)
+//     sum+=i
+// }   
+
+// var sum=0
+// for(var i=1;i<500;i+=2)
+//     {
+        
+//         console.log(sum,"+",i,"=",sum+i)
+//         sum+=i
+        
+//  
+// var counter=5
+// while (counter) {
+//     console.log("Hello",counter)
+//     counter++    
+// }
+
+
+
+
+
+
+// var sum=0
+// for(var i=2;i<=500;i+=2)
+//     {
+//         console.log(sum,"+",i,"=",sum+i)
+//         sum+=i
+//     }
+
+// var sum=0
+// for (var i=1; i<=6;i++) {
+    
+//     console.log(sum,"+",i,"=",sum+i)
+
+//     sum+=i
+    
+// }
+
