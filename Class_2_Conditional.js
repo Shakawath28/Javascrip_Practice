6a6281d98384797b572598c14e762c0e83c7a3a2
Shakawath28 <shakawath132@gmail.com>
2024-09-06 01:04:03 +0600

    Uploaded second file

---
+++ Class_2_Conditional.js
@@ -0,0 +1,26 @@
+ //var prompt = require("prompt-sync")();
+// var name=prompt("Enter Your name:");
+// if (name=="Tamim") {
+//     console.log("Welcome", name)
+    
+// }
+// else if (name=="Asif") {
+//     console.log("Welcome", name)
+    
+// } else {
+//     console.log("Sorry! We don't find you in the system.")
+    
+// }
+
+//Practise
+// var num1=prompt("enter first number:");
+// remainder=num1%3
+// console.log("the remainder is:",remainder)
+
+
+
+
+
+
+
+
