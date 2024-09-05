764968ab9ebf6cd0511c697635b2f1672818191c
Shakawath28 <shakawath132@gmail.com>
2024-09-06 01:04:41 +0600

    Uploaded third file

---
+++ Class3.js
@@ -0,0 +1,94 @@
+// var prompt = require('prompt-sync')();
+// var name=prompt("Enter Your name:")
+
+// if (name=="Jhon"||name=="Raihan"||name=="Paul") {
+//     console.log("Welcome")
+    
+// } else {
+//     console.log("Sorry! you are not authorized")
+//     }
+
+// 
+// var num=Number(prompt("Enter your number="));
+// if (num<40) {
+//     console.log("Your grade is F")
+    
+// }
+// else if (num>=40 && num<50) {
+//     console.log("Your grade is E")
+// } 
+// else if (num>=50 && num<60) {
+//     console.log("Your grade is D")
+    
+// } 
+//Loop
+// for (let i = 1;true; i++) {
+//     console.log(i)
+    
+// }
+
+// 
+// 
+//
+//Loop
+// var a=Number(prompt("Enter a number="))
+// a++
+// console.log(a)
+
+/// For Loop
+// for(var i=0;i>-100;i+=-10)
+//     {
+//         console.log(i,"Tamim")
+//     }
+
+// for(var i=1;i<=100;i++)
+//     {
+//         console.log(i)
+//     }
+//var i=Number(prompt("Enter e initial value="))
+// for(var i=0;i<100;i++)
+//     {
+//         if (i%2==1) {
+//             console.log(i)
+//         }
+        
+        
+//     }
+
+// var z=0
+// for(var i=1;i<=10;i++)
+//     {
+//         console.log(z,"+",i, "=" ,z+i)
+//         z+=i
+           
+//     }
+
+//
+// var sum=0
+// for(var i=1;i<=10;i++)
+//     {
+//         sum+=i
+//         console.log(sum)
+//     }
+
+// var sum=0
+// for(var i=1;i<=500;i+=2)
+//     {
+//         console.log(sum , "+", i , "=" , sum+i)
+//         sum+=i
+        
+//     }
+//
+// var a=Number(prompt("enter the first number of the series= "))
+// var n=Number(prompt("enter the nth term= "))
+// var d=Number(prompt("enter the common difference of the series= "))
+// var nth_term=a+(n-1)*d
+// console.log(n,"th term of the series is: ",nth_term)
+// var sum=0
+// for(i=3;i<=498;i+=5)
+//  {
+    
+//     console.log(sum,"+",i,"=",sum+i)
+//     sum+=i
+//  }
+
